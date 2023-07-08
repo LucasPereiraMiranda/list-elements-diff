f5f5bdfd6f6294d91dbeece65954c0efd66387c3
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2023-07-08 08:32:27 -0300

    feat: handle diff resource

---
+++ src/handle.py
@@ -0,0 +1,16 @@
+from list_diff import ListDiff
+
+
+def handle():
+    list_1 = ['1', '2', '3']
+    list_2 = ['1', '2']
+
+    list_diff = ListDiff(list_1, list_2)
+    result = list_diff.calculate_diff()
+    print('list_1:',list_1)
+    print('list_2:',list_2)
+    print('diff:',result)
+
+
+if __name__ == '__main__':
+    handle()
